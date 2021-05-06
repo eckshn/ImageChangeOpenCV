53fb88613be6c4b09025778970a307db9afc66a4
Eckart Schneider <eckshn@gmail.com>
2021-05-05 21:52:12 -0400

    add up till contour

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,29 @@ import org.opencv.imgproc.Imgproc;
 public class Main {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		String picture = "src/rbhog.jpg";
+		String picture = "src/drip.jpg";
 		
 		Mat image = loadImage(picture);
-		Mat output = new Mat();
-		
+		Mat thresh = new Mat();
+		Mat grayscale = new Mat();
 		//Switches RGB value (Must be RGB2...) to BGR (Similar to cv2.cvtColor from video)
-		Imgproc.cvtColor(image, output, Imgproc.COLOR_RGB2BGR); //sets the value of output of the desired change from image 
-		saveImage(output, "src/output/gray.jpg"); //saves output as a file
-		
+		Imgproc.cvtColor(image, grayscale, Imgproc.COLOR_RGB2GRAY); //sets the value of output of the desired change from image 
+		saveImage(grayscale, "src/output/gray.jpg"); //saves output as a file
 		showImage(picture);
 		showImage("src/output/gray.jpg");
+		
+		Imgproc.threshold(grayscale, thresh, 180, 255, Imgproc.THRESH_BINARY_INV);
+		saveImage(thresh, "src/output/thresh.jpg");
+		showImage("src/output/thresh.jpg");
+		
+		Mat kernel = Mat.ones(new int[] {5, 5}, 0);
+		Mat opening = new Mat();
+		Mat closing = new Mat();
+		Imgproc.morphologyEx(thresh, opening, Imgproc.MORPH_OPEN, kernel);
+		Imgproc.morphologyEx(opening, closing, Imgproc.MORPH_CLOSE, kernel);
+		saveImage(closing, "src/output/closing.jpg");
+		showImage("src/output/closing.jpg");
+		
 	}
 	
 	//Loads the given image as a Mat(Matrix Representation)
